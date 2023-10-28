592c9125b7f51edd94558542a9bee17173486960
Vandit Bhatnagar <30599026+Golubhat@users.noreply.github.com>
2023-10-28 23:08:53 +0530

    Add files via upload

---
+++ Windows Application/Source Code/Program.cs
@@ -0,0 +1,16 @@
+namespace The_Diary;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.Run(new Diary());
+    }    
+}
